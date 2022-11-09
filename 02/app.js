fdb3cc2f09ee5eb0d283157bf50ee82d7fe43844
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-09 15:23:29 +0100

    while loop

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,15 @@
 const x = 5;
 let result = 0;
 
-for(let i=1; i<=x; i++) {
+// for(let i=1; i<=x; i++) {
+//     result= result + i;
+//     console.log(result);
+// }
+// console.log(result);
+
+let i = 1;
+while(i<=5) {
     result= result + i;
-    console.log(result);
+    i++;
 }
 console.log(result);
