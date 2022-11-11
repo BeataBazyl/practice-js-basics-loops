132f9e0324736bcade2b7250793f7293ce00f53d
Beata Bazyl <beata.m.bazyl@gmail.com>
2022-11-11 18:27:58 +0100

    task 03

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,17 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+// let i = 1;
+// while(i<=5) {
+//     result= result + i;
+//     i++;
+// }
+// console.log(result);
+
+while( randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20);
+    iteration = iteration + 1;
+    console.log(randomNumber, iteration)
+}
+console.log(iteration);
